3cad060c1683b9e2e0f924f838d1e092a24afb1e
SamCouto <samcouto2003@protonmail.com>
2024-11-25 22:14:00 -0300

    Minor changes

--- filesBackup.sh
+++ filesBackup.sh
@@ -3,13 +3,25 @@
 # ==========================================
 # | Author:            Samuel Couto        |
 # | Creation Date:     12.11.2024          |
-# | Last Modification: 23.11.2024          |
+# | Last Modification: 26.11.2024          |
 # | Description:       Create file backups |
 # | Usage:             ./filesBackup.sh    |
 # ==========================================
 
+# Validates if the ~/Backups/ directory exists
+function checkDir {
+  if [[ -d "$HOME"/Backups/ ]]
+  then
+    main
+  else
+    mkdir "$HOME"/Backups/
+    main
+    exit 0
+  fi
+}
+
 function exitValidation {
-  while [ true ]; do
+  while true ; do
     read -rp "Do you want to exit? (Y/n): " vStrOpt
     if [[ ${vStrOpt^^} = "N" ]]
     then
@@ -20,7 +32,6 @@ function exitValidation {
       clear
       exit
     else
-      clear
       continue
       exit 0
     fi
@@ -30,7 +41,6 @@ function exitValidation {
 # Lists all the .tar backup files in the backup directory
 function backupsList {
   echo "Reading your $HOME/Backups/ directory..."
-  sleep 1
 
   cd "$HOME"/Backups/ || return
 
@@ -54,15 +64,18 @@ function backup {
   if [[ -d "$vStrDir" ]]
   then
     echo "Your backup is starting..."
-    tar -cvf "$HOME"/Backups/files_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar "$vStrDir"* 2>/dev/null  
+
+    tar -cvf "$HOME"/Backups/files_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar "$vStrDir"* 2>/dev/null 
+
     echo "Your backup is ready at $HOME/Backups/"
     sleep 2
+
     clear
     main
   else
     echo "Please, insert a valid directory!"
     sleep 2
-    clear
+
     backup
     exit 0
   fi
@@ -77,30 +90,29 @@ function main {
 
   read -rp "Hello $USER! What would you like to do? " vIntOpt
 
-  if [[ $vIntOpt -eq 1 ]]
-  then
-    clear
-    backup
-  elif [[ $vIntOpt -eq 2 ]]
-  then
-    clear
-    backupsList
-  elif [[ -z $vIntOpt && $vIntOpt -ne 1 && $vIntOpt -ne 2 ]]
-  then
-    clear
-    main
-  else
-    clear
-    exit 0
-  fi
+  case $vIntOpt in
+
+    0)
+      clear
+      exit 0
+      ;;
+
+    1)
+      clear
+      backup
+      ;;
+
+    2)
+      clear
+      backupsList
+      ;;
+
+    *)
+      clear
+      main
+      ;;
+
+  esac
 }
 
-# Validates if the ~/Backups/ directory exists
-if [[ -d "$HOME"/Backups/ ]]
-then
-  main
-else
-  mkdir "$HOME"/Backups/
-  main
-  exit 0
-fi
+checkDir
